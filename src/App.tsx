59841c1a4327ed50d4299e115c903cfcd053925b
mccree.li <mccree.li@cloudwise.com>
2022-06-22 15:19:10 +0800

    测试通知 弹框

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,35 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { Routes, Route } from "react-router-dom";
+import Dialog from "./pages/dialog";
+import { Button } from "@mui/material";
+import {
+  isPermissionGranted,
+  requestPermission,
+  sendNotification,
+} from "@tauri-apps/api/notification";
+import { invoke } from "@tauri-apps/api";
+const App = () => {
+  const send = async () => {
+    let permissionGranted = await isPermissionGranted();
+    if (!permissionGranted) {
+      const permission = await requestPermission();
+      permissionGranted = permission === "granted";
+    }
+    if (permissionGranted) {
+      sendNotification("Hello world");
+      sendNotification({ title: "TAURI", body: "Tauri is awesome!" });
+    }
+  };
+  const inv = () => {
+    invoke("greet", { name: "world" }).then((resp) => {
+      console.log(resp);
+      // sendNotification(resp)
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <Button variant="contained" color="primary"onClick={send}>Send</Button>
+      <Button onClick={inv}>Invoke</Button>
     </div>
-  )
-}
-
-export default App
+  );
+};
+export default App;
